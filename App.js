0bd5774ac3a172e99312ab1a1ec3906abd8343f0
Prashanth H T <Prashanth.HT@ycs.in>
2025-02-11 11:57:38 +0530

    episode01

---
+++ App.js
@@ -0,0 +1,14 @@
+{/* <div parent>
+    <div child>
+        <h1></h1>
+    </div>
+</div> */}
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h2", { id: "heading" }, "Hello From React!"), React.createElement("h3", { id: "heading" }, "Hello From React2!")), React.createElement("div", { id: "child" },
+            React.createElement("h2", { id: "heading" }, "Hello From React!"), React.createElement("h3", { id: "heading" }, "Hello From React2!")));
+// react.createElement will return one object and render method will take this object and create 
+// tag andd add it into dom
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);

cf825eb5b3b179c6c3455413fa47976d65c0211a
Prashanth H T <Prashanth.HT@ycs.in>
2025-02-13 17:06:10 +0530

    episode03firsthalf

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-{/* <div parent>
-    <div child>
-        <h1></h1>
-    </div>
-</div> */}
 import React from "react";
 import ReactDOM from "react-dom/client"
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        React.createElement("h2", { id: "heading" }, "Hello From React!"), React.createElement("h3", { id: "heading" }, "Hello From React2!")), React.createElement("div", { id: "child" },
-            React.createElement("h2", { id: "heading" }, "Hello From React!"), React.createElement("h3", { id: "heading" }, "Hello From React2!")));
-// react.createElement will return one object and render method will take this object and create 
-// tag andd add it into dom
+
+const JSXheading = <h2 id="heading">Hello From JSX!</h2>
+const heading = React.createElement("h2", { id: "heading" }, "Hello From React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(
+    <>{JSXheading}{heading}</>);
+
+
